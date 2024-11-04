761584de935dbca6620b32bd5ee71b29cb2caacd
marcelocunhaejovem <marcelocunhaejovem@gmail.com>
2024-11-04 15:04:34 -0300

    Update urls.py

--- frequencia/urls.py
+++ frequencia/urls.py
@@ -5,16 +5,8 @@ from controle_frequencia import views as controle_views  # Importa views do app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='controle_frequencia/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', controle_views.home, name='home'),  # URL para a página inicial
     path('home/', controle_views.home, name='home'),  # URL para a página "home"
-]
-
-from django.contrib.auth import views as auth_views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='controle_frequencia/login.html'), name='login'),
-    # outros caminhos...
 ]
