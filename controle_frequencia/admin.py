8ee1e84dc085a14a78e8b9c2359cd98a673b3f83
marcelocunhaejovem <marcelocunhaejovem@gmail.com>
2024-11-04 16:14:59 -0300

    Update admin.py

--- controle_frequencia/admin.py
+++ controle_frequencia/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Estudante, Turma, Professor, Frequencia
 
-# Register your models here.
+# Registrando os modelos
+admin.site.register(Estudante)
+admin.site.register(Turma)
+admin.site.register(Professor)
+admin.site.register(Frequencia)
