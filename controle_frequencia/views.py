fdd7d2c938209f3bb7214f48ca4789d759875e38
marcelocunhaejovem <marcelocunhaejovem@gmail.com>
2024-11-06 14:55:56 -0300

    Update views.py

--- controle_frequencia/views.py
+++ controle_frequencia/views.py
@@ -1,10 +1,11 @@
 # controle_frequencia/views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from .forms import RegistroForm
 
+@login_required
 def home(request):
-    # Esta é a view para a página inicial
     return render(request, 'controle_frequencia/home.html')
 
 def registro(request):
