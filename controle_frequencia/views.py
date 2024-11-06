b49afc0a5d582d6452af3f97c24d26cf238df076
marcelocunhaejovem <marcelocunhaejovem@gmail.com>
2024-11-06 11:26:49 -0300

    Update views.py

--- controle_frequencia/views.py
+++ controle_frequencia/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import RegistroForm
 
+def home(request):
+    # Esta é a view para a página inicial
+    return render(request, 'controle_frequencia/home.html')
+
 def registro(request):
     if request.method == 'POST':
         form = RegistroForm(request.POST)
@@ -11,7 +15,7 @@ def registro(request):
             user.set_password(form.cleaned_data['password'])
             user.save()
             login(request, user)  # Autentica o usuário automaticamente após o registro
-            return redirect('dashboard')  # Redireciona para o dashboard
+            return redirect('home')  # Redireciona para a página inicial após o registro
     else:
         form = RegistroForm()
-    return render(request, 'registro.html', {'form': form})
+    return render(request, 'controle_frequencia/registro.html', {'form': form})
