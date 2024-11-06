741b5348d466012c09625b5e35483811aa21820d
marcelocunhaejovem <marcelocunhaejovem@gmail.com>
2024-11-06 10:40:52 -0300

    Create forms.py

---
+++ controle_frequencia/forms.py
@@ -0,0 +1,19 @@
+# controle_frequencia/forms.py
+from django import forms
+from django.contrib.auth.models import User
+
+class RegistroForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            self.add_error('confirm_password', "As senhas não coincidem.")
