e304796f0b29558c5523540fa8873dc111922638
marcelocunhaejovem <marcelocunhaejovem@gmail.com>
2024-11-06 10:36:48 -0300

    Update apps.py

--- controle_frequencia/apps.py
+++ controle_frequencia/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class ControleFrequenciaConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "controle_frequencia"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'controle_frequencia'
+
+    def ready(self):
+        import controle_frequencia.signals  # Importa os sinais para registrar grupos e permissões
+
