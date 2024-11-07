8d12854579da86d766fb94c5d58cb9c79631dab0
marcelocunhaejovem <marcelocunhaejovem@gmail.com>
2024-11-07 14:05:40 -0300

    Update forms.py

--- controle_frequencia/templates/controle_frequencia/forms.py
+++ controle_frequencia/templates/controle_frequencia/forms.py
@@ -1,1 +1,6 @@
 
+# controle_frequencia/forms.py
+from django import forms
+
+class UploadTurmaForm(forms.Form):
+    arquivo = forms.FileField(label='Selecione o arquivo de turmas (CSV, XLS, XLSX)', required=True)
