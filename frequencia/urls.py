e19023d4db83061198d2363522a884f3b8fa94b8
marcelocunhaejovem <marcelocunhaejovem@gmail.com>
2024-11-07 10:12:18 -0300

    Update urls.py

--- frequencia/urls.py
+++ frequencia/urls.py
@@ -10,13 +10,12 @@ urlpatterns = [
     path('', controle_views.home, name='home'),  # URL para a página inicial
     path('home/', controle_views.home, name='home'),  # URL para a página "home"
     path('registro/', controle_views.registro, name='registro'),  # URL para o registro
+    path('turmas/', controle_views.turmas, name='turmas'),  # URL para a página de turmas
+    path('frequencia/', controle_views.frequencia, name='frequencia'),  # URL para a página de frequência
 
     # URLs para redefinição de senha
     path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
-    # Adicione a URL para "turmas"
-    path('turmas/', controle_views.turmas, name='turmas'),  # Nova URL para a view "turmas"
 ]
